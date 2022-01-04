c07e1d33dbd568cd7d21ac2a9fce24638f7d8b8d
Matt Gallucci <mattgallucci97@gmail.com>
2022-01-04 14:02:30 -0800

    added NFT metadata and minting functionality

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
-
+// Jakck the Shepsky metadata: https://jsonkeeper.com/b/DF96
 pragma solidity ^0.8.0;
-
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
+contract MyEpicNFT is ERC721URIStorage {
+    // keep track of token IDs with OpenZeppelin 
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor() ERC721 ("SquareNFT", "SQUARE")  {
         console.log("Yo my contract is on the block(chain)");
+    }
+
+    // mint function
+    function makeAnEpicNFT() public {
+        // get the current token ID starting from 0
+        uint256 newItemId = _tokenIds.current();
+        // actually mint the NFT to the sender using msg.sender
+        _safeMint(msg.sender, newItemId);
+        // set the NFTs data
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/DF96");
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+        // increment the counter for the IDs
+        _tokenIds.increment();
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -3,6 +3,15 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to ", nftContract.address);
+
+    // call the function to mint the NFT 
+    let txn = await nftContract.makeAnEpicNFT();
+    // wait for it to be mined
+    await txn.wait();
+    // mint another just because
+    txn = await nftContract.makeAnEpicNFT();
+    // wait for it to be mined
+    await txn.wait();
 };
 
 const runMain = async () => {
